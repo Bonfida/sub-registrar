d5ca7f178b44a73f68c233ba6d26fd1dd6022063
DR497 <47689875+dr497@users.noreply.github.com>
2024-01-23 16:12:16 +0800

    fix build script

--- program/build.sh
+++ program/build.sh
@@ -12,11 +12,11 @@ solana program dump metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s target/deploy/mp
 
 if [[ ${1} == "build-only" ]]; then
     echo "Only building..."
-    docker run -it --net=host --mount type=bind,source=$(pwd),target=/workdir sub_register:latest /bin/bash -c "cargo build-sbf"
+    docker run -it --net=host --mount type=bind,source=$(pwd),target=/workdir --mount type=bind,source=$SSH_AUTH_SOCK,target=/ssh-agent --env SSH_AUTH_SOCK=/ssh-agent sub_register:latest /bin/bash -c "cargo build-sbf"
 elif [[ ${1} == "test" ]]; then
     echo "Running tests..."
-    docker run -it --net=host --mount type=bind,source=$(pwd),target=/workdir sub_register:latest /bin/bash -c "cargo test-sbf"
+    docker run -it --net=host --mount type=bind,source=$(pwd),target=/workdir --mount type=bind,source=$SSH_AUTH_SOCK,target=/ssh-agent --env SSH_AUTH_SOCK=/ssh-agent sub_register:latest /bin/bash -c "cargo test-sbf"
 else
     echo "Running tests + building..."
-    docker run -it --net=host --mount type=bind,source=$(pwd),target=/workdir sub_register:latest /bin/bash -c "cargo test-sbf && cargo build-sbf"
+    docker run -it --net=host --mount type=bind,source=$(pwd),target=/workdir --mount type=bind,source=$SSH_AUTH_SOCK,target=/ssh-agent --env SSH_AUTH_SOCK=/ssh-agent sub_register:latest /bin/bash -c "cargo test-sbf && cargo build-sbf"
 fi
